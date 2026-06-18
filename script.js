7788110c6adaf394f0dad2c9709f00a86c15495c
Zhiqian27 <zhiqian026@gmail.com>
2026-06-18 19:34:24 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,677 @@
+// ==============================
+// 毕业生资料
+// 以后新增同学，只需要复制一个对象即可
+// ==============================
+
+const graduates = {
+
+    yufan: {
+
+        name: "林宇凡",
+
+        music: "music/yufan.mp3",
+
+        message: `
+
+愿你始终保持热爱，
+
+奔赴下一场山海。
+
+愿未来一路繁花相伴，
+
+毕业快乐！
+
+
+`
+
+    },
+
+    YaoMing: {
+
+        name: "刘耀铭",
+
+        music: "music/yufan.mp3",
+
+        message: `
+
+愿所有努力都有回报，
+
+愿所有梦想都有归处。
+
+祝你前程似锦！
+
+
+`
+
+    },
+
+    YiJing: {
+
+        name: "郑宇静",
+
+        music: "music/yijing.mp3",
+
+        message: `
+
+愿未来的每一天，
+
+都闪闪发光。
+
+愿你一直做自己喜欢的人。
+
+毕业快乐！
+
+`
+
+    },
+
+    ZHONGYAN: {
+
+        name: "谢忠谚",
+
+        music: "music/zhongyan.mp3",
+
+        message: `
+
+愿少年乘风，
+
+一路向前。
+
+未来可期，
+
+毕业快乐！
+
+`
+
+    },
+
+    Harry: {
+
+        name: "刘俊衔",
+
+        music: "music/harry.mp3",
+
+        message: `
+
+愿所有努力都有回报，
+
+所有梦想都有归处
+
+`
+
+    },
+
+    Angel: {
+
+        name: "符恩绮",
+
+        music: "music/angel.mp3",
+
+        message: `
+
+愿你带着大学里收获的一切，
+
+去往更广阔的天地，
+
+闪闪发光！
+
+
+`
+
+    },
+
+    Jolin: {
+
+        name: "尤静宜",
+
+        music: "music/jolin.mp3",
+
+        message: `
+
+祝你未来遇见的每一个人，
+
+都像你一样真诚、善良,
+
+毕业快乐！
+
+
+
+`
+
+    },
+
+    oscar: {
+
+        name: "Oscar",
+
+        music: "music/oscar.mp3",
+
+        message: `
+
+Congrats on your graduation! 
+
+May your journey ahead be filled with success and joy
+
+
+
+
+`
+
+    },
+
+    Valerie: {
+
+        name: "Valerie",
+
+        music: "music/Valerie.mp3",
+
+        message: `
+
+Congrats! You've made it! 
+
+May your graduation mark the start of an extraordinary life.
+
+
+
+`
+
+    },
+
+    Syahmi: {
+
+        name: "Syahmi",
+
+        music: "music/Syahmi.mp3",
+
+        message: `
+
+Cheers to your graduation! 
+
+May you chase every dream fearlessly. 
+
+I’ll always support you.
+
+
+`
+
+    },
+
+    KaiJie: {
+
+        name: "朱凯杰",
+
+        music: "music/yufan.mp3",
+
+        message: `
+
+祝你在新的旅程里，
+
+遇见更好的自己，
+
+也遇见更温柔的世界
+ 
+
+`
+
+    },
+
+    YingXuan: {
+
+        name: "胡颖轩",
+
+        music: "music/yingxuan.mp3",
+
+        message: `
+
+愿你所有的坚持，都是因为热爱；
+
+所有的选择，都不留遗憾
+
+
+`
+
+    },
+
+    JunTing: {
+
+        name: "朱瑱诠",
+
+        music: "music/junting.mp3",
+
+        message: `
+
+祝你拥有随时出发的勇气，
+
+也拥有随时停靠的安心
+
+
+`
+
+    },
+
+    JiaYing: {
+
+        name: "李家瑩",
+
+        music: "music/jiaying.mp3",
+
+        message: `
+
+世界很大，但没你走不到的地方
+
+毕业快乐，去闯吧！
+
+
+`
+
+    },
+
+    Jacky: {
+
+        name: "符基业",
+
+        music: "music/jacky.mp3",
+
+        message: `
+
+愿你在未来的战场上，
+
+所向披靡，满载而归
+
+`
+
+    },
+
+    Deon: {
+
+        name: "庄家菘",
+
+        music: "music/deon.mp3",
+
+        message: `
+
+祝你前途无量，光芒万丈，
+
+活成自己想要的模样
+
+`
+
+    },
+
+    TianEn: {
+
+        name: "林天恩",
+
+        music: "music/yufan.mp3",
+
+        message: `
+
+祝你既有仰望星空的理想，
+
+也有脚踏实地的力量
+
+`
+
+    },
+
+    ZHIQIANG: {
+
+        name: "余志强",
+
+        music: "music/zhiqiang.mp3",
+
+        message: `
+
+祝你在没有考试的人生里，
+
+依然逢考必过、事事顺心
+
+
+`
+
+    },
+
+    ChenHee: {
+
+        name: "林亲喜",
+
+        music: "music/junting.mp3",
+
+        message: `
+
+前程似锦，来日方长
+
+毕业快乐，未来可期!
+
+`
+
+    },
+
+    CheeWah: {
+
+        name: "张智华",
+
+        music: "music/cheewah.mp3",
+
+        message: `
+
+山水万程，皆有好运
+
+各自努力，顶峰相见!
+
+
+`
+
+    },
+
+    YuXin: {
+
+        name: "蔡昱欣",
+
+        music: "music/yuxin.mp3",
+
+        message: `
+
+此去经年，不负韶华
+
+愿君前程似锦，再见依旧如故
+
+
+`
+
+    },
+
+    WeiLi: {
+
+        name: "王维莉",
+
+        music: "music/weili.mp3",
+
+        message: `
+
+愿你的青春永不散场，
+
+愿你的热爱永远滚烫!
+
+
+`
+
+    },
+
+    Chris: {
+
+        name: "许颖俐",
+
+        music: "music/chris.mp3",
+
+        message: `
+
+毕业不是结束，
+
+是换一种方式继续发光
+
+
+`
+
+    },
+
+    JINGEN: {
+
+        name: "黄婧恩",
+
+        music: "music/jingen.mp3",
+
+        message: `
+
+愿你带着年少的赤诚，
+
+奔赴下一场山海
+
+`
+
+    },
+
+    RuXin: {
+
+        name: "林如馨",
+
+        music: "music/ruxin.mp3",
+
+        message: `
+
+愿所有努力都有回报，
+
+愿所有梦想都有归处。
+
+祝你前程似锦！
+
+`
+
+        },
+
+    JingRou: {
+
+        name: "张靖柔",
+
+        music: "music/jingrou.mp3",
+
+        message: `
+
+愿未来的每一天，
+
+都闪闪发光。
+
+愿你一直做自己喜欢的人。
+
+毕业快乐！
+
+`
+
+     },
+
+    SongChuan: {
+
+        name: "林松泉",
+
+        music: "music/songchuan.mp3",
+
+        message: `
+
+愿你带着大学里收获的一切，
+
+去往更广阔的天地，
+
+闪闪发光！
+
+`
+
+    },
+
+    WahSheng: {
+
+        name: "张铧升",
+
+        music: "music/wahsheng.mp3",
+
+        message: `
+
+愿你始终保持热爱，
+
+奔赴下一场山海。
+
+愿未来一路繁花相伴，
+
+毕业快乐！
+
+`
+
+    },
+
+    ChiengHuey: {
+
+        name: "蔡沁慧",
+
+        music: "music/chienghuey.mp3",
+
+        message: `
+
+祝你在没有考试的人生里，
+
+依然逢考必过、事事顺心
+
+`
+
+    },
+
+    SzeHsiang: {
+
+        name: "张诗湘",
+
+        music: "music/szehsiang.mp3",
+
+        message: `
+
+祝你在新的旅程里，
+
+遇见更好的自己，
+
+也遇见更温柔的世界
+`
+
+    },
+
+    XianQi: {
+
+        name: "雷贤棋",
+
+        music: "music/xianqi.mp3",
+
+        message: `
+
+山水万程，皆有好运
+
+各自努力，顶峰相见!
+
+`
+
+    },
+
+    LiYi: {
+
+        name: "张立仪",
+
+        music: "music/liyi.mp3",
+
+        message: `
+
+祝你未来遇见的每一个人，
+
+都像你一样真诚、善良,
+
+毕业快乐！
+
+`
+
+    }
+
+};
+
+// ==============================
+// 获取HTML元素
+// ==============================
+
+const home = document.getElementById("home");
+
+const messagePage = document.getElementById("message-page");
+
+const studentName = document.getElementById("student-name");
+
+const message = document.getElementById("message");
+
+const player = document.getElementById("player");
+
+const musicBtn = document.getElementById("music-btn");
+
+const backBtn = document.getElementById("back-btn");
+
+const buttons = document.querySelectorAll(".student-btn");
+
+let playing = false;
+
+// ==============================
+// 点击名字
+// ==============================
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const id = button.dataset.name;
+
+        const data = graduates[id];
+
+        studentName.innerText = "To " + data.name;
+
+        message.innerText = data.message;
+
+        player.src = data.music;
+
+        home.style.display = "none";
+
+        messagePage.style.display = "block";
+
+        player.play();
+
+        playing = true;
+
+        musicBtn.innerText = "⏸ 暂停音乐";
+
+    });
+
+});
+
+// ==============================
+// 音乐按钮
+// ==============================
+
+musicBtn.addEventListener("click", () => {
+
+    if (playing) {
+
+        player.pause();
+
+        playing = false;
+
+        musicBtn.innerText = "🎵 播放音乐";
+
+    } else {
+
+        player.play();
+
+        playing = true;
+
+        musicBtn.innerText = "⏸ 暂停音乐";
+
+    }
+
+});
+
+// ==============================
+// 返回首页
+// ==============================
+
+backBtn.addEventListener("click", () => {
+
+    player.pause();
+
+    player.currentTime = 0;
+
+    playing = false;
+
+    musicBtn.innerText = "🎵 播放音乐";
+
+    messagePage.style.display = "none";
+
+    home.style.display = "block";
+
+});
